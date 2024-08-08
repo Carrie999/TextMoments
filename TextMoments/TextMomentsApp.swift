18abf3d86ec8db89595df38b647317c0c2c2282f
carrie <771685809@qq.com>
2024-08-08 10:48:04 +0800

    Initial Commit

---
+++ TextMoments/TextMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextMomentsApp.swift
+//  TextMoments
+//
+//  Created by  玉城 on 2024/8/8.
+//
+
+import SwiftUI
+
+@main
+struct TextMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
